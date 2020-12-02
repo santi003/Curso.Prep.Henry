12a1b76a5e953635167869f5dd785111d7b8546f
santi003 <santiagojoaquin09@hotmail.com>
2020-12-02 15:55:44 -0300

    Homework 1,2,3 finish

---
+++ 02-JS-I/02-JS-I/homework/homework.js
@@ -0,0 +1,205 @@
+// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
+
+// Crea una variable "string", puede contener lo que quieras:
+const nuevaString ="Santiago";
+
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = 25;
+
+// Crea una variable booleana:
+const nuevoBool = true;
+
+// Resuelve el siguiente problema matemático:
+const nuevaResta = 10 - 5 === 5;
+
+// Resuelve el siguiente problema matemático:
+const nuevaMultiplicacion = 10 * 4 === 40 ;
+
+// Resuelve el siguiente problema matemático:
+const nuevoModulo = 21 % 5 === 1;
+
+
+// En los próximos 22 problemas, deberás completar la función.
+// Todo tu código irá dentro de las llaves de la función.
+// Asegúrate que usas "return" cuando la consola te lo pida.
+// Pista: "console.log()" NO fucionará.
+// No cambies los nombres de las funciones.
+
+function devolverString(str) {
+  // "Return" la string provista: str
+  // Tu código:
+  let palabra = str;
+  return palabra ;
+}
+
+function suma(x, y) {
+  // "x" e "y" son números
+  // Suma "x" e "y" juntos y devuelve el valor
+  // Tu código:
+  return x+y;
+}
+
+function resta(x, y) {
+  // Resta "y" de "x" y devuelve el valor
+  // Tu código:
+  return x-y;
+}
+
+function multiplica(x, y) {
+  // Multiplica "x" por "y" y devuelve el valor
+  // Tu código:
+  return x*y;
+}
+
+function divide(x, y) {
+  // Divide "x" entre "y" y devuelve el valor
+  // Tu código:
+  return x/y;
+}
+
+function sonIguales(x, y) {
+  // Devuelve "true" si "x" e "y" son iguales
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+  return x===y ? true : false ;
+
+
+}
+
+function tienenMismaLongitud(str1, str2) {
+  // Devuelve "true" si las dos strings tienen la misma longitud
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  return str1.length===str2.length ? true : false ;
+}
+
+function menosQueNoventa(num) {
+  // Devuelve "true" si el argumento de la función "num" es menor que noventa
+  // De lo contrario, devuelve "false"
+  // Tu código:
+
+  return num<90 ? true : false;
+}
+
+function mayorQueCincuenta(num) {
+  // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  return num>50 ? true : false;
+}
+
+function obtenerResto(x, y) {
+  // Obten el resto de la división de "x" entre "y"
+  // Tu código:
+
+  return x%y;
+  
+}
+
+function esPar(num) {
+  // Devuelve "true" si "num" es par
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  return num%2===0 ? true : false ;
+}
+
+function esImpar(num) {
+  // Devuelve "true" si "num" es impar
+  // De lo contrario, devuelve "false"
+  // Tu código:
+  return num%2!==0 ? true : false ;
+}
+
+function elevarAlCuadrado(num) {
+  // Devuelve el valor de "num" elevado al cuadrado
+  // ojo: No es raiz cuadrada!
+  // Tu código:
+  return num*num;
+}
+
+function elevarAlCubo(num) {
+  // Devuelve el valor de "num" elevado al cubo
+  // Tu código:
+  return num*num*num;
+}
+
+function elevar(num, exponent) {
+  // Devuelve el valor de "num" elevado al exponente dado en "exponent"
+  // Tu código:
+
+  return Math.pow(num,exponent);
+}
+
+function redondearNumero(num) {
+  // Redondea "num" al entero más próximo y devuélvelo
+  // Tu código:
+  return Math.round(num);
+}
+
+function redondearHaciaArriba(num) {
+  // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
+  // Tu código:
+  return Math.ceil(num);
+}
+
+function agregarSimboloExclamacion(str) {
+  // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
+  // Ejemplo: "hello world" pasaría a ser "hello world!"
+  // Tu código:
+  return str + "!";
+}
+
+function combinarNombres(nombre, apellido) {
+  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
+  // Ejemplo: "Soy", "Henry" -> "Soy Henry"
+  // Tu código:
+  return nombre +" "+ apellido; 
+}
+
+function obtenerSaludo(nombre) {
+  // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
+  // "Martin" -> "Hola Martin!"
+  // Tu código:
+  return "Hola" +" "+ nombre + "!";
+}
+
+function obtenerAreaRectangulo(alto, ancho) {
+  // Retornar el area de un rectángulo teniendo su altura y ancho
+  // Tu código: 
+  return alto*ancho;
+}
+
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  nuevaString,
+  nuevoNum,
+  nuevoBool,
+  nuevaResta,
+  nuevaMultiplicacion,
+  nuevoModulo,
+  devolverString,
+  tienenMismaLongitud,
+  sonIguales,
+  menosQueNoventa,
+  mayorQueCincuenta,
+  suma,
+  resta,
+  divide,
+  multiplica,
+  obtenerResto,
+  esPar,
+  esImpar,
+  elevarAlCuadrado,
+  elevarAlCubo,
+  elevar,
+  redondearNumero,
+  redondearHaciaArriba,
+  agregarSimboloExclamacion,
+  combinarNombres,
+  obtenerSaludo,
+  obtenerAreaRectangulo,
+};
---
+++ 04-JS-III/04-JS-III/homework/homework.js
@@ -0,0 +1,134 @@
+// No cambies los nombres de las funciones.
+
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
+  // Tu código:
+  return array[0];
+}
+
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
+  // Tu código:
+  return array[array.length-1] ; 
+}
+
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+  return array.length;
+}
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  nuevoArray =[];
+  for(let i = 0;i<array.length;i++){
+    nuevoArray[i]=array[i]+1;
+  }
+  return nuevoArray;
+}
+
+function agregarItemAlFinalDelArray(array, elemento) {
+  // Añade el "elemento" al final del array
+  // y devuelve el array
+  // Tu código:
+   array[array.length] = elemento;
+       return array;
+}
+
+function agregarItemAlComienzoDelArray(array, elemento) {
+  // Añade el "elemento" al comienzo del array
+  // y devuelve el array
+  // Pista: usa el método `.unshift`
+  // Tu código:
+  array.unshift(elemento);
+
+  return array;
+}
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+  return palabras.join(' ');
+}
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+  for(let i = 0 ; i <array.length;i++){
+    if(array[i]===elemento){
+      return true;
+    }
+  }
+  return false;
+}
+
+function agregarNumeros(numeros) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor
+  // Tu código:
+  let suma = 0;
+  for(let i = 0 ;i<numeros.length;i++){
+    suma = suma + numeros[i];
+  }
+  return suma;
+
+}
+
+function promedioResultadosTest(resultadosTest) {
+  // "resultadosTest" debe ser una matriz de enteros (int/integers)
+  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+  // Tu código:
+  return agregarNumeros(resultadosTest)/resultadosTest.length;
+
+}
+
+function numeroMasGrande(numeros) {
+  // "numeros" debe ser una matriz de enteros (int/integers)
+  // Devuelve el número más grande
+  // Tu código:
+  let maximo =numeros[0] ;
+  for(let i = 0 ;i<numeros.length;i++){
+    if(numeros[i]>maximo){
+      maximo = numeros[i];
+    }
+  }
+  return maximo;
+}
+
+function multiplicarArgumentos() {
+  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
+  // Si no se pasan argumentos devuelve 0
+  // Si se pasa un argumento, simplemente devuélvelo
+  // Tu código:
+  if(arguments.length<1) return 0;
+  let total = 1
+  for(let i = 0; i < arguments.length; i++) {
+    total = total * arguments[i];
+  }
+  return total;
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  devolverPrimerElemento,
+  devolverUltimoElemento,
+  obtenerLargoDelArray,
+  incrementarPorUno,
+  agregarItemAlFinalDelArray,
+  agregarItemAlComienzoDelArray,
+  dePalabrasAFrase,
+  arrayContiene,
+  agregarNumeros,
+  promedioResultadosTest,
+  numeroMasGrande,
+  multiplicarArgumentos,
+};
